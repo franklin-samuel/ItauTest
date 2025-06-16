3da3b1bf75029daf0f9288f7ed5742f5756b2407
Samuel Franklin <samuelfranklinff@gmail.com>
2025-06-16 17:25:05 -0300

    Basic structure

---
+++ transacoes/transacoes/src/main/kotlin/com/itau/desafio/transacoes/TransacoesApplication.kt
@@ -0,0 +1,11 @@
+package com.itau.desafio.transacoes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TransacoesApplication
+
+fun main(args: Array<String>) {
+	runApplication<TransacoesApplication>(*args)
+}
---
+++ transacoes/transacoes/src/test/kotlin/com/itau/desafio/transacoes/TransacoesApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.itau.desafio.transacoes
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TransacoesApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
