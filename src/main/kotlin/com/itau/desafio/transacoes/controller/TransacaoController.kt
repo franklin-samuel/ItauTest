43fd53b8d3b357842f322d2a7901d653831d3767
Samuel Franklin <samuelfranklinff@gmail.com>
2025-06-17 07:12:56 -0300

    Get/Set Estatisticas
    Add, del and get estatisticas e adicionando verficações em transacao.kt

--- src/main/kotlin/com/itau/desafio/transacoes/controller/TransacaoController.kt
+++ src/main/kotlin/com/itau/desafio/transacoes/controller/TransacaoController.kt
@@ -5,4 +5,30 @@ import com.itau.desafio.transacoes.service.TransacaoService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import javax.validation.valid
+import org.springframework.validation.annotation.Validated
+import jakarta.validation.Valid
+
+@RestController
+class TransacaoController(private val service: TransacaoService) {
+    @PostMapping("/transacao")
+    fun adicionarTransacao(@RequestBody @Valid transacao: Transacao): ResponseEntity<Void> {
+        return try {
+            if (service.adicionar(transacao)) {
+                ResponseEntity.status(HttpStatus.CREATED).build()
+            } else {
+                ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build()
+            }
+        } catch (e: Exception) {
+            ResponseEntity.badRequest().build()
+        }
+    }
+
+    @DeleteMapping("/transacao")
+    fun limparTransacoes(): ResponseEntity<Void> {
+        service.limpar()
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/estatistica")
+    fun obterEstatisticas() = ResponseEntity.ok(service.calcularEstatisticas())
+}
--- src/main/kotlin/com/itau/desafio/transacoes/model/Transacao.kt
+++ src/main/kotlin/com/itau/desafio/transacoes/model/Transacao.kt
@@ -1,8 +1,14 @@
 package com.itau.desafio.transacoes.model
 
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.PositiveOrZero
 import java.time.OffsetDateTime
 
 data class Transacao(
+
+    @field:PositiveOrZero(message = "O valor deve ser positivo ou zero")
     val valor: Double,
+
+    @field:NotNull(message = "A data/hora é obrigatória")
     val dataHora: OffsetDateTime
 )
